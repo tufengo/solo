1488efb2afe7d891172c1ed82c05137acb90fb11
tufengo <99628832+tufengo@users.noreply.github.com>
2022-05-04 11:55:49 +0800

    咔咔

--- kk.js
+++ kk.js
@@ -1,12 +1,13 @@
 /*
-我的咔咔邀请码【GI3TR66】
-第1步: 下载：https://h5.imkaka.com/fep/fun-kaka/invitation.html?code=GI3TR66
+我的咔咔邀请码【GDCH5ZS】
+第1步: 下载：https://h5.imkaka.com/fep/fun-kaka/invitation.html?code=GDCH5ZS
 第2步: 填写邀请码
 第3步: 获得5元奖金，可提现到支付宝
 
+抓包https://api.imkaka.com/v1/user/index/inactive?fr=user_......里面的Cookie值
 变量 kkhd='CK'
 多账号@隔开
-kkhd='CK1'@'CK2'
+kkhd='CK1@CK2'
 每日去投票或得现金奖励
 奖励是隔天到账
 注册送5块 需要实名认证提支付宝 秒到
