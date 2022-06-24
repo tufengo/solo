ca9a4da06ba32078eaad9679237c3696393abf03
tufengo <99628832+tufengo@users.noreply.github.com>
2022-06-25 00:13:25 +0800

    58同城

--- 58tc.js
+++ 58tc.js
@@ -1,33 +1,27 @@
 /*
 58同城
-
 安卓貌似需要root才能捉到包，IOS随便捉
 多账号切换账号不能退出登录
-
-手动捉包把PPU=UID=xxxx&UN=yyyy&...填到wbtcCookie里，多账号@隔开
+手动捉包把PPU=UID=xxxx&UN=yyyy&...填到wbtcCookie里，多账号换行隔开
 注意前面有个PPU=，捉包只有UID=xxx的话手动加上
-
 自定义UA：填到wbtcUA里，不填默认IOS15的UA
-
 只做普通任务一天3毛左右，跑小游戏的话一天5毛到6毛
 账号能刷到新手奖励的话每天额外8毛4，前七天还有每天额外3毛(满5提现到矿石)，第一天做完新手任务就能提5块
 先登录，点我的->神奇矿->装扮我的家，过了引导剧情，然后再跑脚本
 游戏赚矿石里的三个小游戏需要投入矿石去赚更多，脚本默认不跑
 如果要跑，在wbtcCookie的对应账号后面加上#1，但是跑久了有可能触发滑块，需要自己去点一次，否则要被反撸矿石
-
 定时不跑小游戏就每天7点后跑5次，跑小游戏就每小时一次
-
 V2P/圈叉：
 [task_local]
 #58同城
-7 * * * * https://raw.githubusercontent.com/leafxcy/JavaScript/main/58tc.js, tag=58同城, enabled=true
+7 7-12 * * * https://raw.githubusercontent.com/leafTheFish/DeathNote/main/58tc.js, tag=58同城, enabled=true
 [rewrite_local]
-https://magicisland.58.com/web/sign/getIndexSignInInfo url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/58tc.js
+https://magicisland.58.com/web/sign/getIndexSignInInfo url script-request-header https://raw.githubusercontent.com/leafTheFish/DeathNote/main/58tc.js
 [MITM]
 hostname = magicisland.58.com
 */
+const $ = new Env("58同城")
 const jsname = '58同城'
-const $ = Env(jsname)
 const logDebug = 0
 
 const notifyFlag = 1; //0为关闭通知，1为打开通知,默认为1
@@ -104,9 +98,9 @@ class UserInfo {
     }
     
     async doTask(sceneId,taskId) {
-        var time = `${(new Date()).getTime()}`
-        var signo = `${time}${taskId}`
-        let url = `https://taskframe.58.com/web/task/dotask?timestamp=${time}&sign=${MD5Encrypt(signo)}&taskId=${taskId}`//&taskData=15`
+        let time = (new Date()).getTime()
+        let sign = MD5Encrypt(`${time}${taskId}`)
+        let url = `https://taskframe.58.com/web/task/dotask?timestamp=${(new Date()).getTime()}&sign=${sign}&taskId=${taskId}`//&taskData=15`
         let body = ``
         let urlObject = populateUrlObject(url,this.cookie,body)
         await httpRequest('get',urlObject)
@@ -121,9 +115,9 @@ class UserInfo {
     }
     
     async getReward(sceneId,taskId) {
-        var time = `${(new Date()).getTime()}`
-        var signo = `${time}${taskId}`
-        let url = `https://taskframe.58.com/web/task/reward?timestamp=${time}&sign=${MD5Encrypt(signo)}&taskId=${taskId}`
+        let time = (new Date()).getTime()
+        let sign = MD5Encrypt(`${time}${taskId}`)
+        let url = `https://taskframe.58.com/web/task/reward?timestamp=${(new Date()).getTime()}&sign=${sign}&taskId=${taskId}`
         let body = ``
         let urlObject = populateUrlObject(url,this.cookie,body)
         await httpRequest('get',urlObject)
@@ -322,7 +316,7 @@ class UserInfo {
                     await $.wait(500)
                     await this.oreGameScore()
                 }
-                console.log(`账号[${this.index}]神奇矿余额${this.ore.ore} ≈ ${this.ore.money}元`)
+                console.log(`账号[${this.index}]神奇矿余额${this.ore.ore} ≈ ${this.ore.money.toFixed(2)}元`)
             }
         } else {
             console.log(`账号[${this.index}]查询神奇矿主页失败: ${result.message}`)
@@ -726,7 +720,7 @@ class UserInfo {
 ///////////////////////////////////////////////////////////////////
 async function checkEnv() {
     if(userCookie) {
-        for(let userCookies of userCookie.split('@')) {
+        for(let userCookies of userCookie.split('\n')) {
             if(userCookies) userList.push(new UserInfo(userCookies))
         }
         userCount = userList.length
